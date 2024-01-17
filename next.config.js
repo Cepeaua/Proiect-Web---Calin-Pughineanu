4eeaf97617c09b889e8e7940426054caa5a44ee6
Cepeaua <pughineanu.ci.calinconstantin22@stud.rau.ro>
2024-01-17 20:14:54 +0200

    first commit

---
+++ next.config.js
@@ -0,0 +1,16 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+
+    experimental: {
+        appDir: true
+    },
+    images: {
+        remotePatterns: [
+            { hostname: "avatars.githubusercontent.com" },
+            { hostname: "lh3.googleusercontent.com" },
+            { hostname: "res.cloudinary.com" }
+        ]
+    }
+}
+
+module.exports = nextConfig
